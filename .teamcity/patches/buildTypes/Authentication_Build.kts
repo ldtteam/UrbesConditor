8ae1230c441f0dcea4b8c89ee86790fd97119ea2
OrionDevelopment <OrionDevelopment@TeamCity>
2022-07-13 06:21:14 +0000

    TeamCity change in 'Let's Dev Together / Authentication' project: build features of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Authentication_Build.kts
+++ .teamcity/patches/buildTypes/Authentication_Build.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
@@ -31,6 +32,14 @@ create(RelativeId("Authentication"), BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_7"
+            }
         }
     }
 }))
