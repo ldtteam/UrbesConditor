6f2aa83801b784ad11801fa3fe0f2256d35db18b
OrionDevelopment <OrionDevelopment@TeamCity>
2023-08-06 11:40:58 +0000

    TeamCity change in 'Let's Dev Together / Minecolonies - Wiki - Old' project: runners of 'Deploy' build configuration were updated

--- .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
+++ .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
@@ -59,7 +59,7 @@ object MinecoloniesWiki_Deploy : BuildType({
         }
         script {
             name = "Deploy wiki chart"
-            workingDir = "charts"
+            workingDir = "chart"
             scriptContent = "helm upgrade --atomic -i -n minecolonies-wiki --set image.tag=${MinecoloniesWiki_BuildAndPublish.depParamRefs.buildNumber} minecolonies-wiki ./wiki"
         }
     }
