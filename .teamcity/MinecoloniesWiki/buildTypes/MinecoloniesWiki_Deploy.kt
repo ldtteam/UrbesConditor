e8d80efd84a6a482226cf400f427460828ba3c74
OrionDevelopment <OrionDevelopment@TeamCity>
2023-08-06 08:58:41 +0000

    TeamCity change in 'Let's Dev Together / Minecolonies - Wiki - Old' project: runners of 'Deploy' build configuration were updated

--- .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
+++ .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
@@ -34,6 +34,7 @@ object MinecoloniesWiki_Deploy : BuildType({
                 	sudo apt-get install -y wget ca-certificates gnupg2
                 
                 	# add repo and signing key
+                    sudo mkdir -p /etc/apt/keyrings/
                     curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
                 	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
                     sudo apt-get update
